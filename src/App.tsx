33a5dfdfa276689e490b4cdad20dc7a9ecc27586
eao.tnc <thanatcha.eaoe@gmaile.ecom>
2021-01-20 03:13:05 +0700

    feat: make snake running right

--- src/App.tsx
+++ src/App.tsx
@@ -3,42 +3,57 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [table, setTable] = useState([{}, 1, 1, 1, 0, 0]);
+  const [snakePosition, setSnakePosition] = useState([
+    { part: "head", x: 2 },
+    { part: "body", x: 1 },
+    { part: "tail", x: 0 },
+  ]);
+  const [tables, setTables] = useState(["", "", "", "", "", ""]);
+  const [keyboardDirection, setKeyboardDirection] = useState("");
   const [snakeLength, setSnakeLength] = useState(3);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      // setTime(prevTime => prevTime + 1); // <-- Change this line!
-      // updateUi();
-      setSnakeLength((prevLength) => prevLength + 1);
+      updateUi();
     }, 1000);
     return () => {
       clearInterval(timer);
     };
   }, []);
 
+  useEffect(() => {
+    let newTable = ["", "", "", "", "", ""];
+    snakePosition.map((snake) => {
+      newTable.splice(snake.x, 1, "s");
+    });
+    setTables(newTable);
+  }, [snakePosition]);
+
   const updateUi = () => {
-    // calculationSnakeLenght()
-    console.log("updateUi", snakeLength);
-    const result = snakeLength + 1;
-    setSnakeLength(result);
+    setSnakePosition((preSnake) =>
+      preSnake.map((snake: any) => {
+        return {
+          part: snake.part,
+          x: snake.x < 5 ? snake.x + 1 : 0,
+        };
+      })
+    );
   };
+  console.log("🚀 ~ useEffect ~ table", tables);
 
   const handleAnswerChange = (event: any) => {
-    updateUi();
-
     switch (event.keyCode) {
       case 37: // left
-        setAnswer("<span>&larr;</span>");
+        setKeyboardDirection("<span>&larr;</span>");
         break;
       case 38: // up
-        setAnswer("&uarr;");
+        setKeyboardDirection("&uarr;");
         break;
       case 39: // right
-        setAnswer("&rarr;");
+        setKeyboardDirection("&rarr;");
         break;
       case 40: // down
-        setAnswer("&darr;");
+        setKeyboardDirection("&darr;");
         break;
       default:
         break;
@@ -46,25 +61,20 @@ function App() {
   };
 
   const renderSnake = () => {
-    let result = [];
-    for (let i = 0; i < snakeLength; i++) {
-      result.push(<div className="row"></div>);
-    }
-    return result;
+    return tables.map((tables) =>
+      tables === "s" ? (
+        <div className="row snake"></div>
+      ) : (
+        <div className="row"></div>
+      )
+    );
   };
 
   return (
     <div className="App">
       <input type="text" onKeyDown={handleAnswerChange} />
       {snakeLength}
-      <div className="column">
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
-      </div>
+      <div className="column">{renderSnake()}</div>
     </div>
   );
 }
