44bcb1b63ba22f8acfd769874011137ab660fe6f
eao.tnc <thanatcha.eaoe@gmaile.ecom>
2021-01-19 22:01:26 +0700

    feat: create setInterval

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [answer, setAnswer] = useState("");
+  const [snakeLength, setSnakeLength] = useState(3);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      // setTime(prevTime => prevTime + 1); // <-- Change this line!
+      // updateUi();
+      setSnakeLength((prevLength) => prevLength + 1);
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const updateUi = () => {
+    // calculationSnakeLenght()
+    console.log("updateUi", snakeLength);
+    const result = snakeLength + 1;
+    setSnakeLength(result);
+  };
+
   const handleAnswerChange = (event: any) => {
+    updateUi();
+
     switch (event.keyCode) {
       case 37: // left
         setAnswer("<span>&larr;</span>");
@@ -23,10 +45,19 @@ function App() {
     }
   };
 
+  const renderSnake = () => {
+    let result: string = "";
+    for (let i = 0; i < snakeLength; i++) {
+      result = result + '<div className="tile"></div>';
+    }
+    return result;
+  };
+
   return (
     <div className="App">
-      {answer}
       <input type="text" onKeyDown={handleAnswerChange} />
+      {snakeLength}
+      {renderSnake()}
     </div>
   );
 }
